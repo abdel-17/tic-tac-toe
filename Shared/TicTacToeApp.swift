8b8c52e829232d018206466e04de8e9d3f304d98
Abdelrahman <88583085+abdel-17@users.noreply.github.com>
2022-10-14 21:52:22 +0200

    Decreased the `minWidth and `minHeight` on macOS. 350 is a usable size and shows the restart button. Added toolbar customization.

--- Shared/TicTacToeApp.swift
+++ Shared/TicTacToeApp.swift
@@ -6,17 +6,12 @@ struct TicTacToeApp: App {
         WindowGroup {
             ContentView()
             #if os(macOS)
-                .frame(minWidth: 400, idealWidth: 400,
-                       minHeight: 400, idealHeight: 400)
+                .frame(minWidth: 350, minHeight: 350)
             #endif
         }
         .commands {
-            CommandMenu("Appearance") {
-                AppearancePicker()
-                    .pickerStyle(.inline)
-                    // Hide the label. The command menu's label is sufficient.
-                    .labelStyle(.iconOnly)
-            }
+            // For customizing the toolbar on macOS.
+            ToolbarCommands()
         }
     }
 }
